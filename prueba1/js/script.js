5a7f9fae7645374735865dc2b8726d95381d6426
Elian Bustos <elianbustos1803@gmail.com>
2023-05-07 17:16:06 -0300

    1 prueba

---
+++ prueba1/js/script.js
@@ -0,0 +1,30 @@
+
+
+//  let mes=prompt("Ingrese su usuario")
+
+
+
+// let nobMes = mes
+// switch (nobMes) {
+//     case 1:
+//         console.log("Enero");
+//         break;
+    
+//     case 2:
+//         console.log("Febrero");
+//         break;
+        
+//     case 3:
+//         console.log("Marzo");
+//         break;
+
+//     default:
+//         console.log("No se encontró el mes");
+//         break;
+// }
+
+
+for (let i = 1; i <= 12; i++) {
+  //i==0, i==1, i==2, i==3
+  console.log( `Valor de i: ${i}`);
+}
